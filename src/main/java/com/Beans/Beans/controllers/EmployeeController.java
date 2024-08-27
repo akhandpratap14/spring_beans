7d5ffb918640b5311e6e517e9f78d82f0a8e1e36
Akhand Pratap Rai <akhandpratap892@gmail.com>
2024-08-27 23:36:51 +0530

    the-presentation-layer-dto-and-controllers

--- src/main/java/com/Beans/Beans/controllers/EmployeeController.java
+++ src/main/java/com/Beans/Beans/controllers/EmployeeController.java
@@ -1,14 +1,12 @@
 package com.Beans.Beans.controllers;
 
 import com.Beans.Beans.dto.EmployeeDTO;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.time.LocalDate;
 
 @RestController
+@RequestMapping(path = "/employees")
 public class EmployeeController {
 
     @GetMapping(path = "/getSecretMessage")
@@ -16,14 +14,24 @@ public class EmployeeController {
         return "Secret: Abracadabra";
     }
 
-    @GetMapping(path = "/getEmployee/{employeeId}")
-    public EmployeeDTO getEmployeeById(@PathVariable Long employeeId){
-        return new EmployeeDTO(employeeId, "Akhand", "akhand@gmail.com", 23, true, LocalDate.of(2023, 9, 15));
+    @GetMapping(path = "/{employeeId}")
+    public EmployeeDTO getEmployeeById(@PathVariable(name = "employeeId") Long id){
+        return new EmployeeDTO(id, "Akhand", "akhand@gmail.com", 23, true, LocalDate.of(2023, 9, 15));
     }
 
-    @GetMapping(path = "/employees")
-    public String getAllEmployees(@RequestParam String name){
-        return "Hii " + name + "!";
+    @GetMapping
+    public String getAllEmployees(@RequestParam(required = false) String name, @RequestParam(required = false) String age){
+        return "Hii " + name + " your age is " + age;
+    }
+
+    @PostMapping
+    public EmployeeDTO createNewEmployee(@RequestBody EmployeeDTO employeeDTO){
+        employeeDTO.setId(2L);
+        return employeeDTO;
+    }
+
+    @PutMapping String updateEmployeeById(){
+        return "employeeDTO";
     }
 
 }
